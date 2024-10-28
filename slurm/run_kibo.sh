bd80defd8a02d941394377d426b1b1d34e9a7ad1
jibanmac <mho026@ucr.edu>
2024-10-27 22:39:51 -0400

    run every pixels in the kibo

---
+++ slurm/run_kibo.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Define the maximum range for the healpix start and end indices
+MAX_HPX_INDEX=16519
+STEP=320
+
+# Loop over the healpix start indices and calculate corresponding end indices
+for (( HPX_START_INDEX=0; HPX_START_INDEX<MAX_HPX_INDEX; HPX_START_INDEX+=STEP )); do
+    HPX_END_INDEX=$((HPX_START_INDEX + STEP))
+
+    # Print the command to be executed for reference
+    echo "sbatch --export=ALL,QSOCAT=\"/global/cfs/cdirs/desi/users/martini/bal-catalogs/kibo/QSO_cat_kibo_main_dark_healpix_v3-altbal.fits\",\
+OUTDIR=\"/pscratch/sd/j/jibancat/desi-kibo-gpdla/\",\
+MAX_DLAS=3,\
+PLOT_FIGURES=0,\
+BATCH_SIZE=313,\
+MAX_WORKERS=32,\
+HPX_START_INDEX=$HPX_START_INDEX,\
+HPX_END_INDEX=$HPX_END_INDEX slurm/submit_desi_kibo.sh"
+
+    # Submit the job using sbatch
+    sbatch --export=ALL,QSOCAT=\"/global/cfs/cdirs/desi/users/martini/bal-catalogs/kibo/QSO_cat_kibo_main_dark_healpix_v3-altbal.fits\",\
+OUTDIR=\"/pscratch/sd/j/jibancat/desi-kibo-gpdla/\",\
+MAX_DLAS=3,\
+PLOT_FIGURES=0,\
+BATCH_SIZE=313,\
+MAX_WORKERS=32,\
+HPX_START_INDEX=$HPX_START_INDEX,\
+HPX_END_INDEX=$HPX_END_INDEX slurm/submit_desi_kibo.sh
+
+    # Sleep for 1 minute before the next sbatch
+    sleep 1m
+
+done
