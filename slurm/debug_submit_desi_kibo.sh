4d702671906b56c0acc2ddb3c0c6c4465e982b8e
jibanmac <mho026@ucr.edu>
2024-11-15 19:02:20 -0500

    change to 8 workers

--- slurm/debug_submit_desi_kibo.sh
+++ slurm/debug_submit_desi_kibo.sh
@@ -10,13 +10,13 @@
 #SBATCH --mail-type=ALL                   # Notification options (ALL = begin, end, fail, etc.)
 #SBATCH -A desi                           # Account name to use on NERSC systems
 #SBATCH --time=00:30:00                   # Time limit for the debug job (30 minutes)
-#SBATCH --ntasks=64                       # 64 tasks total (each running one instance of the Python script)
-#SBATCH --cpus-per-task=4                 # Each task uses 4 CPUs
+#SBATCH --ntasks=32                       # 64 tasks total (each running one instance of the Python script)
+#SBATCH --cpus-per-task=8                 # Each task uses 4 CPUs
 
 # # OpenMP settings for efficient parallel execution
-export OMP_NUM_THREADS=1
-export OMP_PLACES=threads
-export OMP_PROC_BIND=spread
+# export OMP_NUM_THREADS=1
+# export OMP_PLACES=threads
+# export OMP_PROC_BIND=spread
 
 # Ensure the environment is loaded
 source /global/cfs/cdirs/desi/software/desi_environment.sh main
@@ -61,7 +61,7 @@ FIGURE_DIR="${FIGURE_DIR:-/pscratch/sd/j/jibancat/desi-kibo-gpdla-nobal-2_15-7-n
 # Define start and end healpix index ranges for 64 tasks, with each task processing 5 healpix pixels
 HPX_STEP=5
 HPX_START_INDEX="${HPX_START_INDEX:-0}"
-HPX_END_INDEX="${HPX_END_INDEX:-315}"  # 5 healpix pixels * 64 tasks = 320 total healpix pixels
+HPX_END_INDEX="${HPX_END_INDEX:-155}"  # 5 healpix pixels * 32 tasks = 160 total healpix pixels
 
 # Loop over each healpix range and start 8 concurrent jobs
 for (( i = HPX_START_INDEX; i <= HPX_END_INDEX; i += HPX_STEP )); do
@@ -70,7 +70,7 @@ for (( i = HPX_START_INDEX; i <= HPX_END_INDEX; i += HPX_STEP )); do
 
     echo "Running for healpix ${HPX_START} <= HPX < ${HPX_END}"
 
-    srun -N 1 -n 1 -c 4 --output="debug_kibo_run_${HPX_START}-${HPX_END}_%j_%t.log" --error="debug_error_kibo_${HPX_START}-${HPX_END}_%j_%t.log" python desi-DLAGP.py \
+    srun -N 1 -n 1 -c 8 --output="debug_kibo_run_${HPX_START}-${HPX_END}_%j_%t.log" --error="debug_error_kibo_${HPX_START}-${HPX_END}_%j_%t.log" python desi-DLAGP.py \
         --qsocat "$QSOCAT" \
         --release "$RELEASE" \
         --program "$PROGRAM" \
@@ -107,12 +107,12 @@ done
 # Wait for all background jobs to finish
 wait
 
-#     # Submit the job using sbatch
-#     sbatch --export=ALL,QSOCAT="/global/cfs/cdirs/desi/users/martini/bal-catalogs/kibo/QSO_cat_kibo_main_dark_healpix_v3-altbal.fits",\
+# #     # Submit the job using sbatch
+# sbatch --export=ALL,QSOCAT="/global/cfs/cdirs/desi/users/martini/bal-catalogs/kibo/QSO_cat_kibo_main_dark_healpix_v3-altbal.fits",\
 # OUTDIR="/pscratch/sd/j/jibancat/desi-kibo-gpdla-nobal-2_15-7-nozwarn/",\
 # MAX_DLAS=3,\
 # PLOT_FIGURES=1,\
 # BATCH_SIZE=2500,\
-# MAX_WORKERS=4,\
-# HPX_START_INDEX=$HPX_START_INDEX,\
-# HPX_END_INDEX=$HPX_END_INDEX slurm/submit_desi_kibo.sh
+# MAX_WORKERS=3,\
+# HPX_START_INDEX=0,\
+# HPX_END_INDEX=315 slurm/debug_submit_desi_kibo.sh
