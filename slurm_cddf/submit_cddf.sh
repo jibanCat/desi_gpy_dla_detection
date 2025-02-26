23e2915dd782b31adb60de8a9fc3ccaa79cae71f
jibanmac <mho026@ucr.edu>
2025-02-26 11:43:41 -0500

    2025.02.25 : cddf scripts tune

--- slurm_cddf/submit_cddf.sh
+++ slurm_cddf/submit_cddf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=combine_processed_h5     # Job name
-#SBATCH --output=%x_%j.out                  # Standard output log (%j expands to job ID)
-#SBATCH --error=%x_%j.err                   # Standard error log
+#SBATCH --job-name=cddf                      # Job name
+#SBATCH --output=cddf_%j.out                 # Standard output log (%j expands to job ID)
+#SBATCH --error=cddf_%j.err                  # Standard error log
 #SBATCH --time=00:30:00                      # Time limit
 #SBATCH --nodes=1                            # Use one node
 #SBATCH -C cpu                               # Use CPU node
