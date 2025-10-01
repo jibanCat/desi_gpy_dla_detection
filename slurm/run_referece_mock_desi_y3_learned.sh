567e9635c4c13ad7b6403d5c4b4a8c6e402e9bc4
jibanmac <mho026@ucr.edu>
2025-09-30 22:07:21 -0400

    2025.09.30 : production run of single absorber

--- slurm/run_referece_mock_desi_y3_learned.sh
+++ slurm/run_referece_mock_desi_y3_learned.sh
@@ -11,9 +11,9 @@ for (( START_INDEX=0; START_INDEX<=MAX_START_INDEX; START_INDEX+=STEP )); do
     # Print the command to be executed for reference
     echo "sbatch --export=ALL,QSOCAT=\"/global/cfs/projectdirs/desi/mocks/lya_forest/london/qq_desi_y3/v5.9.5/mock-0/jura-124/zcat.fits\",\
 MOCKDIR=\"/global/cfs/projectdirs/desi/mocks/lya_forest/london/qq_desi_y3/v5.9.5/mock-0/jura-124/\",\
-OUTDIR=\"/pscratch/sd/j/jibancat/desi-mock-gpdla-20250912-y3-learned-epoch920-filter/\",\
+OUTDIR=\"/pscratch/sd/j/jibancat/desi-mock-gpdla-20250930-y3-learned-epoch920-filter-single-nhi198/\",\
 LEARNED_FILE=\"/pscratch/sd/j/jibancat/desi_gpy_dla_detection/learnlogs/model_epoch_920.h5\",\
-DLA_SAMPLES_FILE=\"/pscratch/sd/j/jibancat/desi_gpy_dla_detection/data/dr12q/processed/dla_samples_a03_100000.mat\",\
+DLA_SAMPLES_FILE=\"/pscratch/sd/j/jibancat/desi_gpy_dla_detection/data/dr12q/processed/dla_samples_a10_198_226_100000.mat\",\
 PREV_TAU_0=0.00246,\
 PREV_BETA=3.62,\
 DLAMBDA=0.15,\
@@ -24,6 +24,7 @@ PLOT_FIGURES=0,\
 BATCH_SIZE=12500,\
 MAX_WORKERS=8,\
 FILTER_LOW_LIKELIHOOD=1,\
+SINGLE_ABSORBER_MODEL=1,\
 NUM_DLA_SAMPLES=100000,\
 START_INDEX=$START_INDEX,\
 END_INDEX=$END_INDEX,\
@@ -32,9 +33,9 @@ STEP=2 slurm/submit_desi_mock.sh"
     # Submit the job using sbatch
     sbatch --export=ALL,QSOCAT="/global/cfs/projectdirs/desi/mocks/lya_forest/london/qq_desi_y3/v5.9.5/mock-0/jura-124/zcat.fits",\
 MOCKDIR="/global/cfs/projectdirs/desi/mocks/lya_forest/london/qq_desi_y3/v5.9.5/mock-0/jura-124/",\
-OUTDIR="/pscratch/sd/j/jibancat/desi-mock-gpdla-20250912-y3-learned-epoch920-filter/",\
+OUTDIR="/pscratch/sd/j/jibancat/desi-mock-gpdla-20250930-y3-learned-epoch920-filter-single-nhi198/",\
 LEARNED_FILE="/pscratch/sd/j/jibancat/desi_gpy_dla_detection/learnlogs/model_epoch_920.h5",\
-DLA_SAMPLES_FILE="/pscratch/sd/j/jibancat/desi_gpy_dla_detection/data/dr12q/processed/dla_samples_a03_100000.mat",\
+DLA_SAMPLES_FILE="/pscratch/sd/j/jibancat/desi_gpy_dla_detection/data/dr12q/processed/dla_samples_a10_198_226_100000.mat",\
 PREV_TAU_0=0.00246,\
 PREV_BETA=3.62,\
 DLAMBDA=0.15,\
@@ -45,6 +46,7 @@ PLOT_FIGURES=0,\
 BATCH_SIZE=12500,\
 MAX_WORKERS=8,\
 FILTER_LOW_LIKELIHOOD=1,\
+SINGLE_ABSORBER_MODEL=1,\
 NUM_DLA_SAMPLES=100000,\
 START_INDEX=$START_INDEX,\
 END_INDEX=$END_INDEX,\
