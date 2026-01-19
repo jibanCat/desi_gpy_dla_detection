141b6b5943895f20a1e632633e33513fb9419202
jibanmac <mho026@ucr.edu>
2026-01-19 11:20:11 -0500

    2026.01.19 : rn for mock 1

--- slurm/lls_runs/run_reference_mock1_nhi172.sh
+++ slurm/lls_runs/run_reference_mock1_nhi172.sh
@@ -37,29 +37,29 @@ START_INDEX=$START_INDEX,\
 END_INDEX=$END_INDEX,\
 STEP=2 slurm/submit_desi_mock.sh"
 
-#     # Submit the job using sbatch
-#     sbatch --export=ALL,QSOCAT="/global/cfs/projectdirs/desi/mocks/lya_forest/london/qq_desi_y3/v5.9.5/mock-1/jura-124/zcat.fits",\
-# MOCKDIR="/global/cfs/projectdirs/desi/mocks/lya_forest/london/qq_desi_y3/v5.9.5/mock-1/jura-124/",\
-# OUTDIR="/pscratch/sd/j/jibancat/desi-mock-1-gpdla-20260119-y3-learned-epoch920-lls_run-nhi172/",\
-# LEARNED_FILE="/pscratch/sd/j/jibancat/desi_gpy_dla_detection/learnlogs/model_epoch_920.h5",\
-# DLA_SAMPLES_FILE="/pscratch/sd/j/jibancat/desi_gpy_dla_detection/data/dr12q/processed/pw_samples_a3_172_220_50000.mat",\
-# SUB_DLA_SAMPLES_FILE="/pscratch/sd/j/jibancat/desi_gpy_dla_detection/data/dr12q/processed/subdla_samples_a03_191_200_100000.mat",\
-# PREV_TAU_0=0.00246,\
-# PREV_BETA=3.62,\
-# DLAMBDA=0.15,\
-# K=30,\
-# MAX_DLAS=1,\
-# NUM_FOREST_LINES=3,\
-# PLOT_FIGURES=0,\
-# BATCH_SIZE=6250,\
-# MAX_WORKERS=8,\
-# FILTER_LOW_LIKELIHOOD=0,\
-# SINGLE_ABSORBER_MODEL=1,\
-# NUM_DLA_SAMPLES=50000,\
-# NUM_SUBDLA_SAMPLES=100000,\
-# START_INDEX=$START_INDEX,\
-# END_INDEX=$END_INDEX,\
-# STEP=2 slurm/submit_desi_mock.sh
+    # Submit the job using sbatch
+    sbatch --export=ALL,QSOCAT="/global/cfs/projectdirs/desi/mocks/lya_forest/london/qq_desi_y3/v5.9.5/mock-1/jura-124/zcat.fits",\
+MOCKDIR="/global/cfs/projectdirs/desi/mocks/lya_forest/london/qq_desi_y3/v5.9.5/mock-1/jura-124/",\
+OUTDIR="/pscratch/sd/j/jibancat/desi-mock-1-gpdla-20260119-y3-learned-epoch920-lls_run-nhi172/",\
+LEARNED_FILE="/pscratch/sd/j/jibancat/desi_gpy_dla_detection/learnlogs/model_epoch_920.h5",\
+DLA_SAMPLES_FILE="/pscratch/sd/j/jibancat/desi_gpy_dla_detection/data/dr12q/processed/pw_samples_a3_172_220_50000.mat",\
+SUB_DLA_SAMPLES_FILE="/pscratch/sd/j/jibancat/desi_gpy_dla_detection/data/dr12q/processed/subdla_samples_a03_191_200_100000.mat",\
+PREV_TAU_0=0.00246,\
+PREV_BETA=3.62,\
+DLAMBDA=0.15,\
+K=30,\
+MAX_DLAS=1,\
+NUM_FOREST_LINES=3,\
+PLOT_FIGURES=0,\
+BATCH_SIZE=6250,\
+MAX_WORKERS=8,\
+FILTER_LOW_LIKELIHOOD=0,\
+SINGLE_ABSORBER_MODEL=1,\
+NUM_DLA_SAMPLES=50000,\
+NUM_SUBDLA_SAMPLES=100000,\
+START_INDEX=$START_INDEX,\
+END_INDEX=$END_INDEX,\
+STEP=2 slurm/submit_desi_mock.sh
 
     # Sleep for 1 minute before the next sbatch
     sleep 1m
